a5ec976d2ef7a8a6ba0b8bb8dcb70ef54fb12a7e
@aceron <jceron@apexsystems.com>
2022-07-05 18:36:27 -0500

    Dropdown's implemented

--- src/app/components/crud-main-component/components/crud-form-component/crud-form-component.component.ts
+++ src/app/components/crud-main-component/components/crud-form-component/crud-form-component.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthorModel } from 'src/app/models/author-model';
+import { EditorialModel } from 'src/app/models/editorial-model';
+import { LanguageModel } from 'src/app/models/language-model';
 
 @Component({
   selector: 'app-crud-form-component',
@@ -6,8 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crud-form-component.component.css']
 })
 export class CrudFormComponentComponent implements OnInit {
+  languages: LanguageModel[];
+  authors: AuthorModel[];
+  editorials: EditorialModel[];
 
-  constructor() { }
+  constructor() {
+    this.languages = [{
+      id:0,
+      value:'-'
+    },{
+      id: 1,
+      value: 'en-US'
+    }, {
+      id: 2,
+      value: 'es-MX'
+    }];
+
+    this.authors = [{
+      id:0,
+      name:'-'
+    },{
+      id: 1,
+      name: 'William Shakespeare'
+    }, {
+      id: 2,
+      name: 'Stephen King'
+    }];
+
+    this.editorials = [{
+      id:0,
+      name:'-'
+    },{
+      id: 1,
+      name: 'Mirahadas'
+    }, {
+      id: 2,
+      name: 'Errara Natura'
+    }];
+  }
 
   ngOnInit(): void {
   }
---
+++ src/app/models/editorial-model.ts
@@ -0,0 +1,4 @@
+export class EditorialModel {
+	id!:number;
+	name!:string;
+}
