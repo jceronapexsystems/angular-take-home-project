60a563280e11d5a9ad7246a8291fda122043fb2f
@aceron <jceron@apexsystems.com>
2022-08-04 17:40:14 -0500

    Files removed an relocated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<app-crud-main></app-crud-main>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
